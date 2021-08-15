7663f29b3cf4978868f2acc8ee63bf30da0b90fe
phonemelar <phonemelartanvongphab@gmail.com>
2021-08-15 21:21:46 +0700

    add new text

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello SavanitDev')
+  res.send('Hello SavanitDev Say Hello!')
 })
 
 app.listen(port, () => {
